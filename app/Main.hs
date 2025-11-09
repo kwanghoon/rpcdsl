9da9475e24a28d2ef147d238fe25a82457c10d08
Kwanghoon Choi <lazyswamp@gmail.com>
2025-11-09 17:51:16 +0900

    client test

--- app/Main.hs
+++ app/Main.hs
@@ -24,3 +24,42 @@ client = do
   -- QuasiQuoter style:  [$rpc| at hS { incS 41 } |]  ≡  callH hS incS 41
   y  <- [rpc| at hS { incS 41 } |]
   pure y
+
+{-
+
+For a client incS, the Haskell typechecker
+will produce type errors!
+
+incS :: Int -> RPC 'Client Int
+incS n = pure (n+1)
+
+app\Main.hs:23:18: error:
+    * Couldn't match type 'Client with 'Server
+      Expected: Int -> RPC 'Server Int
+        Actual: Int -> RPC 'Client Int
+    * In the second argument of `callH', namely `incS'       
+      In a stmt of a 'do' block: x <- callH hS incS 41       
+      In the expression:
+        do hS <- spawn SServer "srv-img"
+           x <- callH hS incS 41
+           y <- (((callH hS) incS) 41)
+           pure y
+   |
+23 |   x  <- callH hS incS 41
+   |                  ^^^^
+
+app\Main.hs:25:14: error:
+    * Couldn't match type 'Client with 'Server
+      Expected: Int -> RPC 'Server Int
+        Actual: Int -> RPC 'Client Int
+    * In the second argument of `callH', namely `incS'       
+      In a stmt of a 'do' block: y <- (((callH hS) incS) 41) 
+      In the expression:
+        do hS <- spawn SServer "srv-img"
+           x <- callH hS incS 41
+           y <- (((callH hS) incS) 41)
+           pure y
+   |
+25 |   y  <- [rpc| at hS { incS 41 } |]
+
+-}
